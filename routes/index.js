6bf6014f4471e682cb1894860a21e50389245a81
Armando <xxnowonxx@gmail.com>
2018-11-04 23:54:57 -0800

    updated files

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const flash = require("connect-flash");
 
 var isAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) { 
@@ -11,27 +10,19 @@ var isAuthenticated = function (req, res, next) {
   res.redirect('/');
 }
 
-  // API Routes 
-  router.use("/api", apiRoutes);
+// API Routes 
+router.use("/api", apiRoutes);
 
-  // If no API routes are hit, send the React app
-  router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-  });
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 
-  /* Handle Login POST */
-  // router.post('/', passport.authenticate('login', {
-  //   successRedirect: '/user',
-  //   failureRedirect: '/',
-  //   failureFlash : true 
-  // }));
- 
-
- router.use('/user', isAuthenticated, function(req, res){
-   console.log("working?", req.user)
-    res.send({ user: req.user });
- });
+router.use('/user', isAuthenticated, function(req, res){
+  console.log("working?", req.user)
+  res.send({ user: req.user });
+});
 
  
 
